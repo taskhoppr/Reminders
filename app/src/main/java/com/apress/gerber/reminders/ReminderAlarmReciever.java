83696e21cca60698ca5a5f6f62754c1405cf6f42
Shashank Kumar <shashank.kr@gmail.com>
2015-08-14 04:35:48 -0500

    adds broadcast receiver for alarm

---
+++ app/src/main/java/com/apress/gerber/reminders/ReminderAlarmReciever.java
@@ -0,0 +1,17 @@
+package com.apress.gerber.reminders;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by shashank.kumar on 8/13/2015.
+ */
+public class ReminderAlarmReciever extends BroadcastReceiver{
+    public static final String REMINDER_TEXT = "Reminder text";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
